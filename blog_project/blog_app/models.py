d8fe2f76d2349cda38a21f8bc4e54fd3307294df
RoxyYujinKim <yujinkim0707@gmail.com>
2023-09-12 19:54:15 +0900

    [Fix] Models 수정

--- blog_project/blog_app/models.py
+++ blog_project/blog_app/models.py
@@ -3,56 +3,39 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericRelation
+from ckeditor_uploader.fields import RichTextUploadingField
+from django.contrib.postgres.fields import ArrayField  
 
-# Create your models here.
 
-# Category 모델
+class User(models.Model):
+    user_pwd = models.CharField(max_length=20)
+    user_email = models.CharField(max_length=100)
+    user_name = models.CharField(max_length=20)
+    user_login_date = models.DateTimeField(auto_now_add=True)
+
 class Category(models.Model):
-    name=models.CharField(max_length=100)
+    category_name = models.CharField(max_length=30)
 
-    def __str__(self):
-        return self.name
-    
-# 태그 모델
-class Tag(models.Model):
-    name=models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-    
-# Post 모델
 class BlogPost(models.Model):
-    title=models.CharField(max_length=200)
-    content=models.TextField()
-    category=models.ForeignKey(Category,on_delete=models.CASCADE)
-    date=models.DateTimeField(auto_now_add=True)
-    tags=models.ManyToManyField('Tag',blank=True)
-    like=GenericRelation('Like',related_query_name='post')
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = RichTextUploadingField(blank=True,null=True)
+    category_name = models.ForeignKey(Category, on_delete=models.CASCADE)
+    upload_date = models.DateTimeField(auto_now_add=True)
+    tags = ArrayField(models.CharField(max_length=300), blank=True, null=True)
+    like_count = models.IntegerField()
+    status = models.BooleanField(default=False)
+    imag = models.TextField()
 
 
-# Like 모델
-class Like(models.Model):
-    user=models.ForeignKey(User,on_delete=models.CASCADE)
-    post=models.ForeignKey(BlogPost,on_delete=models.CASCADE)
-
-    class Meta:
-        unique_together = ('user', 'post')  # 하나의 사용자가 같은 포스트에 여러 번 좋아요를 누르지 못하도록 합니당ㅇ
-
-# Comment 모델
 class Comment(models.Model):
-    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
-    like=GenericRelation('Like',related_query_name='comment')
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    blog_Post_id = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
+    comment = models.TextField()
+    chrmt_upload_date = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f"Comment by {self.user.username}"
-
-# Views 모델
-class Views(models.Model):
-    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    session_key = models.CharField(max_length=40, null=True, blank=True)
-    ip_address = models.GenericIPAddressField(null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+class Like(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    blog_Post_id = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
+    comment_id = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    like_upload_date = models.DateTimeField(auto_now_add=True)
