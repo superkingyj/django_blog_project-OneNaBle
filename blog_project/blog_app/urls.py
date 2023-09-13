5335c630ecfc6afb8148ad1f6e744beb54653f51
RoxyYujinKim <yujinkim0707@gmail.com>
2023-09-13 17:38:01 +0900

    [Feat] post_detail CRUD 기능 구현

--- blog_project/blog_app/urls.py
+++ blog_project/blog_app/urls.py
@@ -3,13 +3,25 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
 
+post_list = views.BlogPostViewSet.as_view({
+    'get': 'list',
+    'post': 'create',
+})
+
+post_detail = views.BlogPostViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'delete': 'destroy',
+})
+
+
 router = DefaultRouter()
 router.register(r"post", views.BlogPostViewSet)
-router.register(r"user", views.UserViewSet)
-
 
 urlpatterns = [
     path("api/", include(router.urls)),
+    path("api/post", post_list),
+    path("api/post/<int:pk>", post_detail),
     
     path("", views.board_client, name="board_client"),
     path("login", views.custom_login, name="login"),
